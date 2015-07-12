0d74761c5884ac73eb54a508dcb2e61b0487b698
stefanobazzi <stefanobazzi@yahoo.it>
2015-07-12 15:58:30 +0200

    module to manage data, added classes User and Contacts

---
+++ contact_book_data.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from os import getcwd
+from os.path import exists 
+import sqlite3
+
+class User(object):
+
+    def __init__(self, name, surname, job):
+        self.name = name
+        self.surname = surname
+        self.job = job
+
+
+class Contacts(object):
+
+    def __init__(self):
+        self.users = []
+        self.db = None
+
+    def init_db(self):
+        """Create a new sqlite DB if not exits else load data"""
+        if exists(getcwd()+'/data.db'):
+            self.db = sqlite3.connect('data.db')
+            users = self.db.execute("SELECT * FROM users").fetchall()
+            self.data = [User(u[0], u[1], u[2]) for u in users]
+        else:
+            self.db = sqlite3.connect('data.db')
+            self.db.execute(
+                "CREATE TABLE users (name text, surname, text, job text)")
+
+    def add_contact(self, user):
+        self.users.append(user)
+        self.db.execute(
+            "INSERT INTO users VALUES (?, ?, ?)", (user.name, user.surname, user.job))
+
+    def search_contact(self, name, surname=None, job=None):
+        #user = self.db.execute("SELECT * FROM users WHERE (name=?)", name).fetchall()
+        user_list = [u for u in users if u.name == name]
+        return user_list
